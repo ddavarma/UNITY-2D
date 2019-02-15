701b8288fbb6e3267e31be9ee98eebf0f753e5ab
Dantuluri Devi Aditya Varma <ddavarma@gmail.com>
2019-02-15 20:51:45 +0530

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float move;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            move = -0.3f;
+        }
+    }
+    void Start()
+    {
+        move = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(move, 0));
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D cl;
+    Animator ad;
+    public GameObject bulletpos, rightbullet;
+
+    void Start()
+    {
+        cl = GetComponent<Rigidbody2D>();
+        ad = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        ad.SetBool("Walk", false);
+        ad.SetBool("Shoot", false);
+        //ad.SetBool("Runshoot", false);
+        float move = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxis("Jump");
+        cl.velocity = new Vector2(move  * 5f, jump*10);
+        if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)))
+        {
+
+            //if (Input.GetKey(KeyCode.G) && Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+            //{
+            //    ad.SetBool("Walk", false);
+            //    ad.SetBool("Runshoot", true);
+            //    Instantiate(rightbullet, bulletpos.transform.position, bulletpos.transform.rotation);
+            //}
+            ad.SetBool("Walk", true);
+            //ad.SetBool("Runshoot", true);
+            //Instantiate(rightbullet, bulletpos.transform.position, bulletpos.transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ad.SetBool("Shoot", true);
+            Instantiate(rightbullet, bulletpos.transform.position, bulletpos.transform.rotation);
+        }
+
+        //if (Input.GetKey(KeyCode.F))
+        //        {
+        //    ad.SetBool("Runshoot", true);
+        //    Instantiate(rightbullet, bulletpos.transform.position, bulletpos.transform.rotation);
+        //}
+    }
+   
+}
